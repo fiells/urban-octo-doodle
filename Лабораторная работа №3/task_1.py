c812750f1671701df3f260627dad46fe860d6d7f
fiells <148498121+fiells@users.noreply.github.com>
2023-11-12 23:08:32 +0300

    Лабораторная работа №3

---
+++ Лабораторная работа №3/task_1.py
@@ -0,0 +1,17 @@
+# TODO Напишите функцию для поиска индекса товара
+def find_index_item(items_list, find_item):
+    for index, items in enumerate(items_list):
+        if items == find_item:
+            return index
+
+
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_index_item(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
